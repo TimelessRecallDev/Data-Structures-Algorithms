b65178e26a6b2a434b08abf8dd64688338b0b58a
Timeless <timelessrecall@gmail.com>
2025-06-06 13:19:37 +0530

    Added fib using memoizaton

---
+++ Recursion/Fibonacci/Fib-recursion-memoization.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int fib_memo (int n)   //Reduced the TC from O(2^N) to O(N)
+{
+    static int F[10] = {0};  
+    
+    if (n <= 1) {
+        F[n] = n;
+        return n;
+    }
+    else
+    {
+        if (F[n - 1] == 0) {
+            F[n - 1] = fib_memo(n - 1);  
+        }
+        
+        if (F[n - 2] == 0) {
+            F [n - 2] = fib_memo (n -2);
+        }
+        
+        return F [n -1] + F[ n -2];
+    }
+}
+int main()
+{
+    int n; 
+    
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    
+    cout << "Result : " << fib_memo(n);
+}
