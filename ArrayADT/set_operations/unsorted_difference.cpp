2115723e564944c9ee1ce778c9ea2a6d6c093639
Timeless <timelessrecall@gmail.com>
2025-06-29 19:18:31 +0530

    Added difference for unsorted array

---
+++ ArrayADT/set_operations/unsorted_difference.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> set_difference(vector<int> arr1, vector<int> arr2) {
+    vector<int> result;
+
+    for (int i = 0; i < arr1.size(); i++) {
+        bool found = false;
+        for (int j = 0; j < arr2.size(); j++) {
+            if (arr1[i] == arr2[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            result.push_back(arr1[i]);
+        }
+    }
+
+    return result;
+}
+
+int main(){
+    vector <int> arr1 = {32,34,51,45,2};
+    vector <int> arr2 = {20,34,10,2,30};
+    
+    vector <int > arr3 = set_difference (arr1, arr2);
+    
+    for (auto val : arr3) cout << val << " " ;
+}
