60b28a26ee4239c7413280ee51f561d6b8a5a813
Timeless <timelessrecall@gmail.com>
2025-06-30 18:05:19 +0530

    added finding_counting_duplicates

--- ArrayADT/student_challenge/finding_counting_duplicates.cpp
+++ ArrayADT/student_challenge/finding_counting_duplicates.cpp
@@ -26,6 +26,36 @@ void counting_duplicates (int arr[], int length){
     }
 }
 
+void unsorted_find_count_duplicates (int arr[], int n){
+    
+    int count;
+    for (int i = 0 ; i < n  -1 ; i ++)
+    {
+            count = 1;
+            if (arr[i] != -1)
+              for (int j = i+ 1;  j < n ; j++){
+                if (arr[i] == arr[j]){
+                    count++;
+                    arr[j] = -1;
+                }
+        }
+        if (count > 1)cout << arr[i] << " appeared " << count << " times." << endl;
+    }
+}
+
+void hashing_find_count_duplicates (int arr[], int length, int max){
+    int H[max + 1] = {0};
+    
+    for (int i =0 ; i < length ;i++){
+        H[arr[i]]++;
+    }
+    
+    for (int i = 0 ; i <= max ; i++){
+        if (H[i] > 1)
+          cout << i << " appeared " << H[i] << " times" << endl;
+    }
+}
+
 int main(){
     int arr[10] = {6,6,8, 8, 9, 10 ,11 ,11 ,11,12};
     
