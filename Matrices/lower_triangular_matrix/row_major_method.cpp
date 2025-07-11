acf62b9d3429491bc66f699f74a5445d04b2644f
Timeless <timelessrecall@gmail.com>
2025-07-11 19:35:39 +0530

    Added lower triangular matrix

---
+++ Matrices/lower_triangular_matrix/row_major_method.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class Matrix {
+private:
+    int n;
+    int* arr;
+
+public:
+    Matrix(int n) {
+        this->n = n;
+        arr = new int[(n * (n + 1)) / 2];
+    }
+
+    void set(int i, int j, int value) {
+        if (i >= j) {
+            arr[(i * (i - 1)) / 2 + (j - 1)] = value;
+        }
+    }
+
+    int get(int i, int j) {
+        if (i >= j) {
+            return arr[(i * (i - 1)) / 2 + (j - 1)];
+        } else {
+            return 0;
+        }
+    }
+
+    void display() {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                cout << get(i, j) << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    ~Matrix() {
+        delete[] arr;
+    }
+};
+
+int main() {
+    int n, value;
+
+    cout << "Enter the dimension: ";
+    cin >> n;
+
+    Matrix m(n);
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cin >> value;
+            m.set(i, j, value);
+        }
+    }
+
+    cout << "Lower Triangular Matrix:\n";
+    m.display();
+
+    return 0;
+}
